a6d9294419e88ebdb0963b34ae500d3bd1f71f28
islam-shahpar <si2382@email.vccs.edu>
2023-02-11 11:06:24 -0500

    Initial Commit

---
+++ MediumApp/MediumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumAppApp.swift
+//  MediumApp
+//
+//  Created by Shahpar Islam on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MediumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
